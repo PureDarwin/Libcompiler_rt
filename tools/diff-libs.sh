99de4f856cbee46fcb0d3e2a9c4eaacb7a76860f
William Kent <wjk@users.noreply.github.com>
2017-11-23 19:17:54 -0500

    Add helper shell script
    This script diffs the APIs between Apple's libcompiler_rt
    and the one we just built using the sources in this repo.
    This helps immensely in hunting down differences between
    the two (because Apple's version and ours are built from
    different commits of the upstream LLVM sources).

---
+++ tools/diff-libs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+MY_DIR=$(cd $(dirname $0) && pwd)
+nm -m ${MY_DIR}/sym.nocommit/libcompiler_rt.dylib | sed -Ee 's,[0-9a-f]{16} ,,g' -e '/undefined/d' > ours
+nm -m /usr/lib/system/libcompiler_rt.dylib | sed -Ee 's,[0-9a-f]{16} ,,g' -e '/undefined/d' > apples
+sort ours > ours.sorted
+sort apples > apples.sorted
+diff -u ours.sorted apples.sorted > apis.diff
+
+rm ours ours.sorted apples apples.sorted
